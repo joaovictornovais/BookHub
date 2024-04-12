95fe1e338f244b92042b71ff39e00b5e6c5eb480
João Victor Novais <joao.dipaay@gmail.com>
2024-04-12 13:28:14 -0300

    Update README.md

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <main className="pt-[68px]">
+      <div className="max-w-7xl mx-auto"></div>
+    </main>
+  );
+};
+
+export default Hero;

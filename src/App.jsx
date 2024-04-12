080855ae09519e60ae16ccb777b82e9f60e049fe
João Victor Novais <joao.dipaay@gmail.com>
2024-04-12 13:32:44 -0300

    header com pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>BookHub</div>;
+  const [search, setSearch] = useState("");
+
+  return (
+    <>
+      <Header setSearch={setSearch} />
+    </>
+  );
 };
 
 export default App;
